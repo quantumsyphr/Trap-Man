183e2adb9c45d5be8caa1caa9de6a755bca4c686
quantumsyphr <158118491+quantumsyphr@users.noreply.github.com>
2024-02-21 10:48:46 -0600

    Adds base files for game

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const scoreDisplay = document.getElementById('score');
+    const width = 28
+    let score = 0;
+    const grid = document.querySelector('grid');
+    const layout = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ]
+
+}
